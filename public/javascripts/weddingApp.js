c94ad9ca15f9c3664f8b5b883152390015074a7b
Brendan Heussler <brendan@lifejourney.us>
2015-12-18 00:16:05 -0500

    Add angular strap and scrollspy

--- public/javascripts/weddingApp.js
+++ public/javascripts/weddingApp.js
@@ -1,4 +1,4 @@
-var weddingApp = angular.module('weddingApp', [])
+var weddingApp = angular.module('weddingApp', ['mgcrea.ngStrap.scrollspy', 'mgcrea.ngStrap.helpers.dimensions'])
     .controller('mainController', ['$scope', '$log',
         function ($scope, $log) {
             $scope.test = 'test';

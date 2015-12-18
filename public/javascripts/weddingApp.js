5f7aa6d31beff77fe30ca5184600e9a337e91728
Brendan Heussler <brendan@lifejourney.us>
2015-12-17 22:54:30 -0500

    Change favicon and add angular-bootstrap, replacing bootstrap.js

--- public/javascripts/weddingApp.js
+++ public/javascripts/weddingApp.js
@@ -1,8 +1,8 @@
-var weddingApp;
-weddingApp = angular.module('weddingApp', [])
+var weddingApp = angular.module('weddingApp', [])
     .controller('mainController', ['$scope', '$log',
         function ($scope, $log) {
             $scope.test = 'test';
 
             $log.debug('testing');
-        }]);
+        }])
+    .directive();

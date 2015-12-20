6ecfa72f023227bfcde4e72c735bc160d1c4e099
Brendan Heussler <brendan@lifejourney.us>
2015-12-20 18:55:59 -0500

    Add wedding story and start working on modal

--- public/javascripts/weddingApp.js
+++ public/javascripts/weddingApp.js
@@ -2,6 +2,11 @@ var weddingApp = angular.module('weddingApp', ['ngAnimate', 'mgcrea.ngStrap'])
     .controller('mainController', ['$scope', '$log',
         function ($scope) {
             $scope.activeCollapse = -1;
+
+            $scope.modal = {
+                'title': 'My modal title ',
+                'content': 'Here is some bullshit modal content'
+            }
         }])
     .directive('topSpy', function($window) {
         return {

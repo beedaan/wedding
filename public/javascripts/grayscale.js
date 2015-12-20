3415dce9a15eb5ec2d3189d7c63e33425cb85e6d
Brendan Heussler <brendan@lifejourney.us>
2015-12-20 00:16:51 -0500

    Add topSpy directive.  Working on ngSmoothScroll

--- public/javascripts/grayscale.js
+++ public/javascripts/grayscale.js
@@ -4,24 +4,14 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// TODO put this in angular directive
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
 // TODO have angular handle scrolling
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+//$(function() {
+//    $('a.page-scroll').bind('click', function(event) {
+//        var $anchor = $(this);
+//        $('html, body').stop().animate({
+//            scrollTop: $($anchor.attr('href')).offset().top
+//        }, 1500, 'easeInOutExpo');
+//        event.preventDefault();
+//    });
+//});
--- public/javascripts/weddingApp.js
+++ public/javascripts/weddingApp.js
@@ -1,5 +1,15 @@
-var weddingApp = angular.module('weddingApp', ['ngAnimate', 'mgcrea.ngStrap'])
+var weddingApp = angular.module('weddingApp', ['ngAnimate', 'mgcrea.ngStrap', 'smoothScroll'])
     .controller('mainController', ['$scope', '$log',
         function ($scope) {
             $scope.activeCollapse = -1;
-        }]);
+        }])
+    .directive('topSpy', function($window) {
+        return {
+            link: function(scope) {
+                angular.element($window).bind('scroll', function () {
+                    scope.atTop = this.pageYOffset >= 50;
+                    scope.$apply();
+                });
+            }
+        }
+    });

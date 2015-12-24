6aa97c6a0d22486f6349b78665f00f3afb33e560
Brendan Heussler <brendan@lifejourney.us>
2015-12-24 16:19:10 -0500

    Gallery is "sort of" working.  I really don't like angular-strap

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('weddingApp.controllers', ['mgcrea.ngStrap.modal', 'mgcrea.ngStrap.scrollspy','duScroll']).
-    controller('AppCtrl', function($scope, $log, $modal) {
+angular.module('weddingApp.controllers', ['mgcrea.ngStrap.modal', 'mgcrea.ngStrap.scrollspy','duScroll', 'ngStrapLightbox', 'pascalprecht.translate']).
+    controller('AppCtrl', function($scope, $log, $modal, Lightbox) {
         $log.debug('loaded AppCtrl');
 
         $scope.openRsvpModal = function() {
@@ -9,6 +9,38 @@ angular.module('weddingApp.controllers', ['mgcrea.ngStrap.modal', 'mgcrea.ngStra
                 templateUrl: 'partials/rsvp',
                 controller: 'RsvpCtrl'
             });
+        };
+
+        $scope.images = [
+            {
+                'url': 'images/LHP-84.JPG',
+                'thumbUrl': 'images/LHP-84.JPG'
+            },
+            {
+                'url': 'images/LHP-78.JPG',
+                'thumbUrl': 'images/LHP-78.JPG'
+            },
+            {
+                'url': 'images/LHP-7.JPG',
+                'thumbUrl': 'images/LHP-7.JPG'
+            },
+            {
+                'url': 'images/LHP-45.JPG',
+                'thumbUrl': 'images/LHP-45-landscape.JPG'
+            },
+            {
+                'url': 'images/LHP-131.JPG',
+                'thumbUrl': 'images/LHP-131.JPG'
+            },
+            {
+                'url': 'images/LHP-39.JPG',
+                'thumbUrl': 'images/LHP-39.JPG'
+            }
+        ];
+
+        $scope.openLightboxModal = function (index) {
+            console.log(index);
+            Lightbox.openModal($scope, $scope.images, index);
         }
     }).
     controller('RsvpCtrl', function($scope, $log) {
